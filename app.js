fae66e67a6d87f79c26125ce8a3afacc6a72ef44
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-05-25 01:11:30 +0530

    about contact and compose pages

--- app.js
+++ app.js
@@ -17,13 +17,29 @@ app.use(express.static("public"));
 
 app.get("/",function(req,res){
   res.render("home",{
-    homeContent : homeStartingContent
+    homeStartingContent : homeStartingContent
   });
 });
 
+app.get("/about", function(req,res){
+  res.render("about",{
+   aboutContent : aboutContent
+  });
+});
 
+app.get("/contact", function(req,res){
+  res.render("contact",{
+    contactContent : contactContent
+  })
+});
 
-
+app.get("/compose", function(req,res){
+  res.render("compose");
+});
+ 
+app.post("/compose",function(req,res){
+   console.log(req.body.postTitle);
+ });
 
 
 
